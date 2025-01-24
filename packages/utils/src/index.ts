4320d96f6bba3f06e04eb5bf6175faac50b9f38c
Dmitrii Tkach <dmitrii.tkach@gcore.com>
2025-01-24 18:49:35 +0400

    Add RemoteTracer to the utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,6 @@
 export * from './Logger.js';
 export * from './trace/LogTracer.js';
+export * from './trace/RemoteTracer.js';
 export * from './trace/SentryTracer.js';
 export * from './trace/Tracer.js';
 export * from './trace/index.js';
