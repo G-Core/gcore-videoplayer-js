c9b47a8d9c4c2b211a4e9ef4e7a13f312548f132
Dmitrii Tkach <dmitrii.tkach@gcore.com>
2025-01-28 13:53:27 +0400

    Setup license checks

--- packages/gplayer/src/typings/@clappr/core/playback.d.ts
+++
@@ -1,5 +0,0 @@
-import '@clappr/core';
-
-declare module "@clappr/core" {
-    declare interface Playback extends ErrorMixin { }
-}
--- packages/gplayer/src/typings/globals.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface HTMLVideoElement {
-    webkitDisplayingFullscreen?: boolean;
-}
-
-declare module "*.css";
-declare module "*.scss";
-declare module "*.svg";
-declare module "*.ejs";
